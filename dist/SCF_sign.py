642df854e0f4e6a931abe441388b6eeda3e5c719
jieran233 <jieranjiang233@gmail.com>
2024-05-21 22:48:38 +0800

    Update SCF_sign.py

--- dist/SCF_sign.py
+++ dist/SCF_sign.py
@@ -7,16 +7,25 @@ requests方式
 cookies.json的例子见 https://github.com/Trojblue/TSDM-coin-farmer/blob/main/doc/cookies.json.example
 """
 
-from datetime import datetime
-import random, requests, time, json
+import json
+import requests
+import random
+import time
+import os
+import urllib.parse
+import logging
+
+
+# Set up basic logging configuration
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 # ======== CONSTANT ========
-sign_url = 'https://www.tsdm39.net/plugin.php?id=dsu_paulsign:sign'
-work_url = 'https://www.tsdm39.net/plugin.php?id=np_cliworkdz:work'
-login_url = 'https://www.tsdm39.net/member.php?mod=logging&action=login'
+sign_url = 'https://www.tsdm39.com/plugin.php?id=dsu_paulsign:sign'
+work_url = 'https://www.tsdm39.com/plugin.php?id=np_cliworkdz:work'
+login_url = 'https://www.tsdm39.com/member.php?mod=logging&action=login'
 
-tsdm_domain = ".tsdm39.net"
+tsdm_domain = ".tsdm39.com"
 s1_domain = "bbs.saraba1st.com"
 
 
@@ -27,19 +36,21 @@ def get_cookies_all():
     """从文件读取所有cookies
     { username: [cookie_list] }
     """
+    cookies_json_file = os.path.join(os.path.realpath(os.path.split(__file__)[0]), 'cookies.json')
     try:
-        with open('cookies.json', 'r', encoding='utf-8') as json_file:
+        with open(cookies_json_file, 'r', encoding='utf-8') as json_file:
+            logging.info(f'找到 "{cookies_json_file}"')
             data = json.load(json_file)
             return data
 
     except FileNotFoundError:  # 文件不存在
-        print("cookies.json不存在")
+        logging.error("cookies.json 不存在")
         return {}
 
 
 def get_cookies_by_domain(domain:str):
     """从所有cookie里分离出指定域名的cookie
-    domain: cookie_list domain, (".tsdm39.net")
+    domain: cookie_list domain, (".tsdm39.com")
     """
     cookies_all = get_cookies_all() #     { username: [cookie_list] }
     domain_cookies = {}
@@ -62,20 +73,22 @@ def get_cookies_by_domain(domain:str):
 # ======= SIGN ======
 
 sign_page_with_param = \
-    'https://www.tsdm39.net/plugin.php?id=dsu_paulsign:sign&operation=qiandao&infloat=1&sign_as=1&inajax=1'
+    'https://www.tsdm39.com/plugin.php?id=dsu_paulsign:sign&operation=qiandao&infloat=1&sign_as=1&inajax=1'
 
 
 def sign_single_post_v2(cookie):
     cookie_serialized = "; ".join([i['name'] + "=" + i['value'] for i in cookie])
 
+    logging.info(cookie_serialized)
+
     # 必须要这个content-type, 否则没法接收
     headers = {
-        'accept': 'text/html, application/xhtml+xml, image/jxr, */*',
+        'Accept': 'text/html, application/xhtml+xml, image/jxr, */*',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
-        'cookie_list': cookie_serialized,
-        'connection': 'Keep-Alive',
-        'referer': 'https://www.tsdm39.net/home.php?mod=space&do=pm',
-        'content-type': 'application/x-www-form-urlencoded'
+        'Cookie': cookie_serialized,
+        'Connection': 'Keep-Alive',
+        'Referer': 'https://www.tsdm39.com/home.php?mod=space&do=pm',
+        'Content-Type': 'application/x-www-form-urlencoded'
     }
 
     s = requests.session()
@@ -89,18 +102,16 @@ def sign_single_post_v2(cookie):
     sign_response = s.post(sign_page_with_param, data=sign_data, headers=headers)
 
     if "恭喜你签到成功!获得随机奖励" in sign_response.text:
-        print("签到成功")
+        logging.info("签到成功")
         return True
     elif "您今日已经签到" in sign_response.text:
-        print("该账户已经签到过")
+        logging.warning("该账户已经签到过")
     elif "已经过了签到时间段" in sign_response.text or "签到时间还没有到" in sign_response.text:
-        print("签到失败: 目前不在签到时间段")
+        logging.error("签到失败: 目前不在签到时间段")
     elif "未定义操作" in sign_response.text:
-        print("%s签到失败, 可能是formhash获取错误" % datetime.now())
-        print("签到", sign_response.text)
+        logging.error(f"签到失败, 可能是formhash获取错误\n\n{sign_response.text}")
     else:
-        print("%s======未知原因签到失败, 已保存response=======" % datetime.now())
-        print("签到", sign_response.text)
+        logging.error(f"未知原因签到失败\n\n{sign_response.text}")
 
     return False
 
@@ -109,16 +120,19 @@ def sign_multi_post():
     cookies = get_cookies_by_domain(tsdm_domain)
 
     for user in cookies.keys():
-        print("%s正在签到: %s" % (datetime.now(), user))
+        logging.info(f"正在签到: {user}")
         try:
             sign_single_post_v2(cookies[user])
         except Exception as e:
-            print("%s====post签到出错: %s===" % (datetime.now(), e))
+            logging.error(f"post签到出错: {e}")
 
         time.sleep(random.uniform(0.5, 1))
 
-    print("POST方式: 全部签到完成")
+    logging.info("POST方式: 全部签到完成")
     return
 
 def main_handler(event, context):
+    sign_multi_post()
+
+if __name__ == '__main__':
     sign_multi_post()
